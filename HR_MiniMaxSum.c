f6bf9e1e4428a42ac8122dfddcbb00e06969ee89
Zafor Iqbal <xoy4444@gmail.com>
2023-08-03 18:39:07 +0600

    Update HR_MiniMaxSum.c

--- HR_MiniMaxSum.c
+++ HR_MiniMaxSum.c
@@ -1,18 +1,9 @@
 #include<stdio.h>
 int main(){
-    int arr[15];
+    int arr[5];
     for (int i=0;i<5;i++){
         scanf("%d",&arr[i]);
     }
-
-    int count1=0,count2=0;
-    for (int i=0;i<5-1;i++){
-        count1+=arr[i];
-    }
-    for (int i=1;i<5;i++){
-        count2+=arr[i];
-    }
-    printf("%d ",count1);
-    printf("%d\n",count2);
+    int 
     return 0;
 }
